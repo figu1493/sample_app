a0dbca22ebd11d942611a1ac5ef43cf6854e986a
John Figueiredo <jfigueir33@gmail.com>
2012-09-13 19:52:04 -0700

    Finish sign in

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -26,6 +26,9 @@ describe User do
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
   it { should respond_to(:authenticate) }
+  it { should respond_to(:password_confirmation) }
+  it { should respond_to(:remember_token) }
+  it { should respond_to(:authenticate) }  
 
   it { should be_valid }
 
@@ -57,7 +60,16 @@ describe User do
         @user.should be_valid
       end      
     end
-   
+    # mixed case exercises 6.31 to lowercase all mixed case emails. I made a mistake and it didn't work. Come back to it later to fix!
+    #describe "email address with mixed case" do
+    #let(:mixed_case_email) { "Foo@ExAMPle.CoM" }
+
+    #it "should be saved as all lower-case" do
+      #@user.email = mixed_case_email
+      #@user.save
+     # @user.reload.email.should == mixed_case_email.downcase
+    #end
+    # and exercises
   end
  describe "when email address is already taken" do
     before do
@@ -95,11 +107,17 @@ describe "with a password that's too short" do
       it { should == found_user.authenticate(@user.password) }
     end
 
+
     describe "with invalid password" do
       let(:user_for_invalid_password) { found_user.authenticate("invalid") }
 
       it { should_not == user_for_invalid_password }
       specify { user_for_invalid_password.should be_false }
+    end
+    
+    describe "remember token" do
+      before { @user.save }
+      its(:remember_token) { should_not be_blank }
     end
   end
 end
