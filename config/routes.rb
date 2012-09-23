3263cc2f0a79e57d4039ed9048fd4e8fbfc01939
John Figueiredo <jfigueir33@gmail.com>
2012-09-23 03:49:28 -0700

    Finish user edit, update, index, and destroy actions

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ SampleApp::Application.routes.draw do
   resources :users
   resources :sessions, only: [:new, :create, :destroy]
 
-  #possibly delete. Test system before
   root to: 'static_pages#home'
 
   match '/signup',  to: 'users#new'
