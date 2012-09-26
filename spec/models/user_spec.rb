8f8507ab3f3ec3ebacba04c43015d62f13550ac1
John Figueiredo <jfigueir33@gmail.com>
2012-09-26 05:23:35 -0700

    Add user following

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -34,6 +34,13 @@ describe User do
   it { should respond_to(:authenticate) }
   it { should respond_to(:microposts) }
   it { should respond_to(:feed) }
+  it { should respond_to(:relationships) }
+  it { should respond_to(:followed_users) }
+  it { should respond_to(:reverse_relationships) }
+  it { should respond_to(:followers) }
+  it { should respond_to(:following?) }
+  it { should respond_to(:follow!) }
+  it { should respond_to(:unfollow!) }
 
   it { should be_valid }
   it { should_not be_admin }
@@ -46,7 +53,6 @@ describe User do
 
     it { should be_admin }
   end
-
 
   describe "when name is not present" do
     before { @user.name = " " }
@@ -87,7 +93,7 @@ describe User do
     # and exercises
   end
 
- describe "when email address is already taken" do
+  describe "when email address is already taken" do
     before do
       user_with_same_email = @user.dup
       user_with_same_email.email = @user.email.upcase
@@ -110,7 +116,8 @@ describe User do
     before { @user.password_confirmation = nil }
     it { should_not be_valid }
   end
-describe "with a password that's too short" do
+  
+  describe "with a password that's too short" do
     before { @user.password = @user.password_confirmation = "a" * 5 }
     it { should be_invalid }
   end
@@ -122,7 +129,6 @@ describe "with a password that's too short" do
     describe "with valid password" do
       it { should == found_user.authenticate(@user.password) }
     end
-
 
     describe "with invalid password" do
       let(:user_for_invalid_password) { found_user.authenticate("invalid") }
@@ -137,8 +143,30 @@ describe "with a password that's too short" do
     end
   end
 
-    describe "micropost associations" do
+  describe "following" do
+    let(:other_user) { FactoryGirl.create(:user) }    
+    before do
+      @user.save
+      @user.follow!(other_user)
+    end
 
+    it { should be_following(other_user) }
+    its(:followed_users) { should include(other_user) }
+
+    describe "followed user" do
+      subject { other_user }
+      its(:followers) { should include(@user) }
+    end
+
+    describe "and unfollowing" do
+      before { @user.unfollow!(other_user) }
+
+      it { should_not be_following(other_user) }
+      its(:followed_users) { should_not include(other_user) }
+    end
+  end
+
+  describe "micropost associations" do
     before { @user.save }
     let!(:older_micropost) do 
       FactoryGirl.create(:micropost, user: @user, created_at: 1.day.ago)
@@ -157,15 +185,26 @@ describe "with a password that's too short" do
         Micropost.find_by_id(micropost.id).should be_nil
       end
     end
-    
+
     describe "status" do
       let(:unfollowed_post) do
         FactoryGirl.create(:micropost, user: FactoryGirl.create(:user))
+      end
+      let(:followed_user) { FactoryGirl.create(:user) }
+
+      before do
+        @user.follow!(followed_user)
+        3.times { followed_user.microposts.create!(content: "Lorem ipsum") }
       end
 
       its(:feed) { should include(newer_micropost) }
       its(:feed) { should include(older_micropost) }
       its(:feed) { should_not include(unfollowed_post) }
+      its(:feed) do
+        followed_user.microposts.each do |micropost|
+          should include(micropost)
+        end
+      end
     end
   end
 end
